cd94cbfefc5f13dde4a7828c2c56f3d282ce3a93
eUlisesBaezGt <89673617+eUlisesBaezGt@users.noreply.github.com>
2023-03-27 18:48:14 +0000

    Updates

--- GreedyBestFirstSearch/main.py
+++ GreedyBestFirstSearch/main.py
@@ -9,49 +9,29 @@ class Graph:
             self.content[destiny] = []
         self.content[origin].append((destiny, weight))
         self.content[destiny].append((origin, weight))
-
-    def view_all(self):
-        print("\n\nGraph:\n------")
-        print("ORIGIN -> [(DESTINY, WEIGHT), ...]")
-        for origin, destiny in self.content.items():
-            print(f"{origin} -> {destiny}")
-        print("\n")
-
+        
 
 def greedy_best_first_search(graph, heuristics, start="Neamt", goal="Bucharest"):
     if start == goal:
         return [start]
+    
+    frontier = [start]
+    explored = []
+    path = []
 
-    path = [start]
+    while frontier:
+        node = frontier.pop(0)
+        explored.append(node)
+        path.append(node)
 
-    while True:
-        current = path[-1]
-        children = []
-        heuristic = 0
-        if current == goal:
+        if node == goal:
             return path
 
-        print(f"Current: {current}")
+        for destiny, weight in graph.content[node]:
+            if destiny not in explored and destiny not in frontier:
+                frontier.append(destiny)
 
-        # Get children
-        for node in graph.content[current]:
-            if node[0] not in children:
-                children.append(node[0])
-                for h in heuristics.content[node[0]]:
-                    if h[0] == goal:
-                        heuristic = int(h[1])
-            print(f"Children: {children}")
-            print(f"Heuristic for {node[0]}: {heuristic} ")
-
-        if len(children) == 0:
-            return "No path found."
-
-        # Get the child with the lowest heuristic with lambda function
-        sorted_heuristics = sorted(children, key=lambda x: int(heuristics.content[x][0][1])) # CHECAR PARA EL 0
-        print(f"Sorted Heuristics: {sorted_heuristics}")
-        child_with_lowest_heuristic = sorted_heuristics[0]
-        print(f"Child with lowest heuristic: {child_with_lowest_heuristic}")
-        path.append(child_with_lowest_heuristic)
+        frontier.sort(key=lambda x: heuristics.content[x][0][1])
 
 
 def main():
@@ -74,9 +54,6 @@ def main():
     path = greedy_best_first_search(graph, heuristics)
 
     print(f"Path: {path}")
-
-    # graph.view_all()
-    # heuristics.view_all()
 
 
 if __name__ == "__main__":
---
+++ WeightedAStarSearch/main.py
@@ -0,0 +1,41 @@
+class Graph:
+    def __init__(self):
+        self.content = dict()
+
+    def new_edge(self, origin, destiny, weight):
+        if origin not in self.content:
+            self.content[origin] = []
+        if destiny not in self.content:
+            self.content[destiny] = []
+        self.content[origin].append((destiny, weight))
+        self.content[destiny].append((origin, weight))
+        
+
+def WeightedAStarSeach(graph, heuristics, start="Neamt", goal="Bucharest"):
+    if start == goal:
+        return [start]
+
+def main():
+    graph = Graph()
+    with open("data.txt") as file:
+        lines = file.readlines()
+
+    for i in range(1, len(lines)):
+        origin, destiny, weight = lines[i].split()
+        graph.new_edge(origin, destiny, weight)
+
+    heuristics = Graph()
+    with open("heuristics.txt") as file:
+        lines = file.readlines()
+
+    for i in range(1, len(lines)):
+        origin, destiny, weight = lines[i].split()
+        heuristics.new_edge(origin, destiny, weight)
+
+    path = WeightedAStarSeach(graph, heuristics)
+
+    print(f"Path: {path}")
+
+
+if __name__ == "__main__":
+    main()
